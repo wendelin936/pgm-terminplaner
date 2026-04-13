66cfe2f8c0c090fe4972aa98992292628ad0a721
wendelin936 <wendelin936@gmail.com>
2026-04-13 16:24:01 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAa1RSo21egGdIatqxMtIlbkxO83IJbD2M",
-  authDomain: "paradiesgarten-terminpla-5496c.firebaseapp.com",
-  projectId: "paradiesgarten-terminpla-5496c",
-  storageBucket: "paradiesgarten-terminpla-5496c.firebasestorage.app",
-  messagingSenderId: "763348915903",
-  appId: "1:763348915903:web:cbaf58135c4912adec580a",
-  measurementId: "G-YY76HY540M"
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID
 };
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
