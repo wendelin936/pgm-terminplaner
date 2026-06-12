97730d758e35535dd5d08e43a32132768251ad22
wendelin936 <wendelin936@gmail.com>
2026-06-12 10:41:22 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import App from "./App.jsx";
+
+Sentry.init({
+  dsn: "https://d06f89d2ae2f78688093d9a475013f04@o4511551526928384.ingest.de.sentry.io/4511551540887632",
+  sendDefaultPii: false,        // keine personenbezogenen Daten – DSGVO-konform
+  environment: "production",
+});
 
 createRoot(document.getElementById("root")).render(<App />);
